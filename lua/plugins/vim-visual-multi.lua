3d8dae0246313f6e232f5e386f60e633091cab3c
KevinNitroG <trannguyenthaibinh46@gmail.com>
2024-08-03 11:48:15 +0700

    chore(vim-visual-multi): cleanup

--- lua/plugins/vim-visual-multi.lua
+++ lua/plugins/vim-visual-multi.lua
@@ -2,7 +2,7 @@
 return {
   "mg979/vim-visual-multi",
   event = "User FilePost",
-  init = function(_)
+  init = function()
     vim.g.VM_mouse_mappings = 1
     vim.g.VM_maps = {
       ["Add Cursor Up"] = "<C-PageUp>", -- <C-Up> is for resize windows
