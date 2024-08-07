c8e85a64105f6591f171b94be96d6f7993944bdb
KevinNitroG <trannguyenthaibinh46@gmail.com>
2024-08-07 12:39:05 +0700

    feat(plugins/markdown): change to markview like Alexis
    but lazyload, disable by default

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -2,7 +2,8 @@
 -- NOTE: Better Markdown
 return {
   "MeanderingProgrammer/render-markdown.nvim",
-  ft = "markdown",
+  enabled = false,
+  -- ft = "markdown",
   -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
   dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
   cmd = {
--- lua/plugins/markview.lua
+++ lua/plugins/markview.lua
@@ -2,8 +2,8 @@
 -- NOTE: Better Markdown
 return {
   "OXY2DEV/markview.nvim",
-  lazy = false, -- Recommended
-  -- ft = "markdown" -- If you decide to lazy-load anyway
+  -- lazy = false, -- Recommended
+  ft = "markdown", -- If you decide to lazy-load anyway
   dependencies = {
     -- You will not need this if you installed the
     -- parsers manually
@@ -11,4 +11,20 @@ return {
     "nvim-treesitter/nvim-treesitter",
     "nvim-tree/nvim-web-devicons",
   },
+  cmd = {
+    "Markview",
+  },
+  init = function()
+    vim.keymap.set("n", "<leader>mv", function()
+      if vim.bo.filetype == "markdown" then
+        vim.cmd "Markview"
+      else
+        vim.notify("Only available in markdown", vim.log.levels.WARN, { title = "Markdown-Render" })
+      end
+    end, { desc = "Markview | Toggle View", silent = true })
+  end,
+  config = function(_, opts)
+    require("markview").setup(opts)
+    vim.cmd "Markview disableAll"
+  end,
 }
