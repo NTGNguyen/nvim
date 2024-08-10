0d5587700dbe889a1b640bd0b3411b7d7e1188d2
KevinNitroG <trannguyenthaibinh46@gmail.com>
2024-08-10 22:34:46 +0700

    chore(plugins): add `vim-unimpaired`

---
+++ lua/plugins/vim-unimpaired.lua
@@ -0,0 +1,9 @@
+---@type NvPluginSpec
+-- NOTE: Unimpaired keymaps
+-- "." function not work!
+-- "]o", "[o" is duplicate with treesitter
+return {
+  "tpope/vim-unimpaired",
+  enabled = false,
+  event = "User FilePost",
+}
