e917aa6ad49d4b765f7448bac4385d8199237178
KevinNitroG <trannguyenthaibinh46@gmail.com>
2024-09-07 00:32:07 +0700

    fix(yamlls): not attatch to github action filetype

--- lua/plugins/lsp/settings/yamlls.lua
+++ lua/plugins/lsp/settings/yamlls.lua
@@ -1,10 +1,19 @@
+-- NOTE: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#yamlls
 return {
+  filetypes = {
+    "yaml",
+    "yaml.docker-compose",
+    "yaml.gitlab",
+    "yaml.ghaction", -- Manually add custom filetype
+  },
   settings = {
     yaml = {
       schemaStore = {
         -- You must disable built-in schemaStore support if you want to use
         -- this plugin and its advanced options like `ignore`.
         enable = false,
+        -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
+        url = "",
       },
       schemas = require("schemastore").yaml.schemas(),
     },
