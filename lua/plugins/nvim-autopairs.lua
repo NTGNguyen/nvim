831b14c0faee5efd498be00108e9559b50ded0ed
KevinNitroG <trannguyenthaibinh46@gmail.com>
2024-09-10 18:10:39 +0700

    chore(nvim-autopairs): bruh config

--- lua/plugins/nvim-autopairs.lua
+++ lua/plugins/nvim-autopairs.lua
@@ -11,39 +11,8 @@ return {
   config = function(_, opts)
     require("nvim-autopairs").setup(opts)
 
-    -- If you want insert `(` after select function or method item
     local cmp_autopairs = require "nvim-autopairs.completion.cmp"
     local cmp = require "cmp"
     cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-
-    local handlers = require "nvim-autopairs.completion.handlers"
-
-    cmp.event:on(
-      "confirm_done",
-      cmp_autopairs.on_confirm_done {
-        filetypes = {
-          -- "*" is a alias to all filetypes
-          ["*"] = {
-            ["("] = {
-              kind = {
-                cmp.lsp.CompletionItemKind.Function,
-                cmp.lsp.CompletionItemKind.Method,
-              },
-              handler = handlers["*"],
-            },
-          },
-          lua = {
-            ["("] = {
-              kind = {
-                cmp.lsp.CompletionItemKind.Function,
-                cmp.lsp.CompletionItemKind.Method,
-              },
-            },
-          },
-          -- Disable for tex
-          tex = false,
-        },
-      }
-    )
   end,
 }
