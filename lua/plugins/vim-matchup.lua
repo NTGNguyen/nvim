bc0a6f87fa08a8995596358138090c38b15a4ddc
KevinNitroG <trannguyenthaibinh46@gmail.com>
2024-09-10 10:51:57 +0700

    feat(plugins): add "vim-matchup"

---
+++ lua/plugins/vim-matchup.lua
@@ -0,0 +1,13 @@
+---@type NvPluginSpec
+-- NOTE: vim match-up: even better % 👊 navigate and highlight matching words 👊 modern matchit and matchparen. Supports both vim and neovim + tree-sitter.
+return {
+  "andymass/vim-matchup",
+  init = function()
+    vim.g.matchup_matchparen_offscreen = { method = "popup" }
+  end,
+  opts = {
+    matchup = {
+      enable = true, -- mandatory, false will disable the whole extension
+    },
+  },
+}
