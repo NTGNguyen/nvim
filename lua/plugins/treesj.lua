86a8027a1cfefd97d996baaa6d526e1e44393d84
KevinNitroG <trannguyenthaibinh46@gmail.com>
2024-09-10 10:52:47 +0700

    feat(plugins): add "treesj"

---
+++ lua/plugins/treesj.lua
@@ -0,0 +1,14 @@
+---@type NvPluginSpec
+-- NOTE: Neovim plugin for splitting/joining blocks of code
+return {
+  "Wansmer/treesj",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+  }, -- if you install parsers with "nvim-treesitter"
+  cmd = {
+    "TSJToggle",
+  },
+  init = function()
+    vim.keymap.set("n", "<leader>ut", "<cmd>TSJToggle<cr>", { desc = "TreeSJ | Toggle", silent = true })
+  end,
+}
