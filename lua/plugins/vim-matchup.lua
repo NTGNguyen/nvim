fa0f5f763190496050be35831a9742699dba0283
KevinNitroG <trannguyenthaibinh46@gmail.com>
2024-09-10 18:10:01 +0700

    fix(vim-matchup): bruh config with ts

--- lua/plugins/vim-matchup.lua
+++ lua/plugins/vim-matchup.lua
@@ -2,13 +2,17 @@
 -- NOTE: vim match-up: even better % 👊 navigate and highlight matching words 👊 modern matchit and matchparen. Supports both vim and neovim + tree-sitter.
 return {
   "andymass/vim-matchup",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      matchup = {
+        enable = true, -- mandatory, false will disable the whole extension
+        enable_quotes = true,
+      },
+    },
+  },
   event = "BufReadPost",
   init = function()
     vim.g.matchup_matchparen_offscreen = { method = "popup" }
   end,
-  opts = {
-    matchup = {
-      enable = true, -- mandatory, false will disable the whole extension
-    },
-  },
 }
