c535d0bf6615ac71b0934f28fe716b68ece83f87
KevinNitroG <trannguyenthaibinh46@gmail.com>
2024-09-12 20:48:25 +0700

    feat(minty): add keymaps

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -12,5 +12,23 @@ return {
   { "nvchad/volt" },
   {
     "nvchad/minty",
+    keys = {
+      {
+        "<leader>uh",
+        function()
+          require("minty.huefy").toggle()
+        end,
+        desc = "Minty | Toggle Huefy",
+        silent = true,
+      },
+      {
+        "<leader>us",
+        function()
+          require("minty.shades").toggle()
+        end,
+        desc = "Minty | Shades Huefy",
+        silent = true,
+      },
+    },
   },
 }
