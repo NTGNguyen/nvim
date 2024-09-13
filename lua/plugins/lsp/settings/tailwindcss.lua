8c887c9fd61e1e6d840762cac3503ad18143c526
KevinNitroG <trannguyenthaibinh46@gmail.com>
2024-09-13 18:31:07 +0700

    chore(tailwindcss lsp): only trigger when have tailwindcss config
    also comment out the code class from Alexis

--- lua/plugins/lsp/settings/tailwindcss.lua
+++ lua/plugins/lsp/settings/tailwindcss.lua
@@ -1,7 +1,21 @@
 return {
+  -- NOTE: Just attach tailwindcss LSP when match one of below file
+  root_dir = function(fname)
+    print(fname)
+    return require("lspconfig.util").root_pattern(
+      "tailwind.config.js",
+      "tailwind.config.cjs",
+      "tailwind.config.mjs",
+      "tailwind.config.ts",
+      "postcss.config.js",
+      "postcss.config.cjs",
+      "postcss.config.mjs",
+      "postcss.config.ts"
+    )(fname)
+  end,
   settings = {
     tailwindCSS = {
-      classAttributes = { "class", "className", "ngClass" },
+      -- classAttributes = { "class", "className", "ngClass" },
       experimental = {
         classRegex = {
           "tw`([^`]*)", -- tw`...`
