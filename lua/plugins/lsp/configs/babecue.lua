f75a49ed693b8c365e29f7f70e9aa1b7a852473d
KevinNitroG <trannguyenthaibinh46@gmail.com>
2024-09-14 11:42:28 +0700

    chore(lsp breadcrumb): use "babecue" instead of "lspsaga"

---
+++ lua/plugins/lsp/configs/babecue.lua
@@ -0,0 +1,16 @@
+---@type NvPluginSpec
+-- NOTE: Visual Studio Code inspired breadcrumbs plugin for the Neovim editor
+return {
+  "utilyre/barbecue.nvim",
+  event = "BufReadPost",
+  dependencies = {
+    "SmiteshP/nvim-navic",
+    "nvim-tree/nvim-web-devicons", -- optional dependency
+  },
+  init = function()
+    vim.keymap.set("n", "<leader>lB", function()
+      require("barbecue.ui").toggle()
+    end, { desc = "Barbecue | Toggle Breadcrumbs", silent = true })
+  end,
+  config = true,
+}
