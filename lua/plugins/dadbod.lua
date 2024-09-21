5fd6e8bb5fa509e4491d88687440a9a033a1658b
KevinNitroG <trannguyenthaibinh46@gmail.com>
2024-09-21 23:57:39 +0700

    chore(dadbod): don't execute on save

--- lua/plugins/dadbod.lua
+++ lua/plugins/dadbod.lua
@@ -24,7 +24,7 @@ return {
       "DBUIFindBuffer",
     },
     init = function()
-      -- vim.g.db_ui_execute_on_save = 0
+      vim.g.db_ui_execute_on_save = 0
       vim.g.db_ui_use_nerd_fonts = 1
       vim.g.vim_dadbod_completion_mark = ""
 
