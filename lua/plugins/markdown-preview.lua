75f98bfa9d715ea5ceb32d22e813a4a3ec4bd71d
KevinNitroG <trannguyenthaibinh46@gmail.com>
2024-11-11 19:59:46 +0700

    fix(markdown-preview): keymap trigger on fts

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -1,3 +1,13 @@
+local fts = {
+  "Avante",
+  "help",
+  "markdown",
+  "norg",
+  "org",
+  "rmd",
+  "vimwiki",
+}
+
 ---@type NvPluginSpec
 -- NOTE: Preview Markdown
 return {
@@ -7,8 +17,23 @@ return {
     {
       "<leader>mp",
       "<cmd>MarkdownPreviewToggle<cr>",
-      desc = "Markdown | Preview",
-      ft = "markdown",
+      desc = "Markdown Preview | Toggle",
+      ft = fts,
+      silent = true,
+    },
+    {
+      "<leader>mt",
+      function()
+        if vim.g.mkdp_theme == "light" then
+          vim.g.mkdp_theme = "dark"
+          vim.notify("Dark Mode", vim.log.levels.INFO, { title = "Markdown Preview" })
+        else
+          vim.g.mkdp_theme = "light"
+          vim.notify("Light Mode", vim.log.levels.INFO, { title = "Markdown Preview" })
+        end
+      end,
+      desc = "Markdown Preview | Toggle Theme",
+      ft = fts,
       silent = true,
     },
   },
@@ -16,19 +41,9 @@ return {
     require("lazy").load { plugins = { "markdown-preview.nvim" } }
     vim.fn["mkdp#util#install"]()
   end,
-  ft = "markdown",
+  ft = fts,
   init = function()
     vim.g.mkdp_open_to_the_world = 1 -- Open for other to access
     vim.g.mkdp_port = 65530
-
-    vim.keymap.set("n", "<leader>mt", function()
-      if vim.g.mkdp_theme == "light" then
-        vim.g.mkdp_theme = "dark"
-        vim.notify("Dark Mode", vim.log.levels.INFO, { title = "Markdown Preview" })
-      else
-        vim.g.mkdp_theme = "light"
-        vim.notify("Light Mode", vim.log.levels.INFO, { title = "Markdown Preview" })
-      end
-    end, { desc = "Markdown Preview | Toggle Theme", silent = true })
   end,
 }
