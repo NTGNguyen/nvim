e67e1a5ea90b19b3bdb9f844b91529dfa862eba4
KevinNitroG <trannguyenthaibinh46@gmail.com>
2024-11-24 11:38:41 +0700

    feat(plugins): add "beacon.nvim" (disable)

---
+++ lua/plugins/beacon.lua
@@ -0,0 +1,7 @@
+---@type NvPluginSpec
+-- NOTE: Whenever cursor jumps some distance or moves between windows, it will flash so you can see where it is
+return {
+  "DanilaMihailov/beacon.nvim",
+  enabled = false,
+  event = "BufReadPost",
+}
