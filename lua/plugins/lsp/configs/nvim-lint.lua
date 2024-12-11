204b43bfdf33308e7b1c1642da97c42052b096cf
KevinNitroG <trannguyenthaibinh46@gmail.com>
2024-12-12 00:52:36 +0700

    fix(nvim-lint/eslint): file outside base path

--- lua/plugins/lsp/configs/nvim-lint.lua
+++ lua/plugins/lsp/configs/nvim-lint.lua
@@ -6,7 +6,14 @@ return {
   enabled = true,
   config = function()
     local linters = require("lint").linters
-    vim.list_extend(linters.ruff.args, { "--select=I", "--ignore=F401" })
+    vim.list_extend(linters.ruff.args, {
+      "--select=I",
+      "--ignore=F401",
+    })
+    vim.list_extend(linters.eslint.args, {
+      "--flag",
+      "unstable_config_lookup_from_file", -- for fixing file is ignored because it is located outside of the base path on my Windows
+    })
 
     require("lint").linters_by_ft = {
       -- cpp = { "cpplint" },
@@ -20,6 +27,7 @@ return {
       sh = { "shellcheck" },
       typescript = { "eslint" },
       typescriptreact = { "eslint" },
+      gitcommit = { "commitlint" },
     }
 
     vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost", "InsertLeave" }, {
