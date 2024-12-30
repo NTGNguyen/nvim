4ec475101e8206e13049ad4b5473fa296ee886b8
KevinNitroG <trannguyenthaibinh46@gmail.com>
2024-12-30 22:00:19 +0700

    fix(options): missing delimiter in PATH

--- lua/options.lua
+++ lua/options.lua
@@ -79,6 +79,7 @@ end
 
 -- add binaries installed by mason.nvim to path
 vim.env.PATH = table.concat({ vim.fn.stdpath "data", "mason", "bin" }, vim.g.path_separator)
+  .. vim.g.path_delimiter
   .. table.concat({ vim.fn.stdpath "data", "lazy-rocks", "hererocks", "bin" }, vim.g.path_separator)
   .. vim.g.path_delimiter
   .. vim.env.PATH
